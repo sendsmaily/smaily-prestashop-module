727f7d72b2c0903396b63d93fc04a00d9246ed67
Kaarel <kaarel.rooparg@gmail.com>
2024-04-24 16:15:04 +0300

    Add license headers

--- src/src/Model/RssFeedProductsCollection.php
+++ src/src/Model/RssFeedProductsCollection.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * 2024 Smaily
+ *
+ * NOTICE OF LICENSE
+ *
+ * Smaily for PrestaShop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Smaily for PrestaShop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Smaily for PrestaShop. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Smaily <info@smaily.com>
+ * @copyright 2024 Smaily
+ * @license   GPL3
+ */
+declare(strict_types=1);
 
 namespace PrestaShop\Module\SmailyForPrestaShop\Model;
 
