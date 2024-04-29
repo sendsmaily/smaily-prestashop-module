c97ca9b2a611d79d7989ad40b88349d2b2137f35
Kaarel <kaarel.rooparg@gmail.com>
2024-04-29 11:59:12 +0300

    Fix wrong selector id-s in RSS URL generation script

--- src/views/js/smaily-admin.js
+++ src/views/js/smaily-admin.js
@@ -28,22 +28,22 @@ $(() => {
         var rss_url_base = rssBaseURL + '?';
         var url_parameters = {};
 
-        var rss_limit = $('#rss #form_product_limit').val();
+        var rss_limit = $('#rss #rss_product_limit').val();
         if (rss_limit != "") {
             url_parameters.limit = rss_limit;
         }
 
-        var rss_sort_by = $('#rss #form_sort_by').val();
+        var rss_sort_by = $('#rss #rss_sort_by').val();
         if (rss_sort_by != "") {
             url_parameters.sort_by = rss_sort_by;
         }
 
-        var rss_sort_order = $('#rss #form_sort_order').val();
+        var rss_sort_order = $('#rss #rss_sort_order').val();
         if (rss_sort_order != "") {
             url_parameters.sort_order = rss_sort_order;
         }
 
-        var rss_category_id = $('#rss #form_product_category_id').val();
+        var rss_category_id = $('#rss #rss_product_category_id').val();
         if (rss_category_id != "") {
             url_parameters.category_id = rss_category_id;
         }
