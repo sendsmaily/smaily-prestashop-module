0eebd84f5239f0ef437ffa3dee2c7a32a2e2264a
Kaarel <kaarel.rooparg@gmail.com>
2024-05-22 08:00:13 +0300

    Use regex preg_replace

--- src/src/Model/AbandonedCartCollection.php
+++ src/src/Model/AbandonedCartCollection.php
@@ -54,8 +54,8 @@ class AbandonedCartCollection
      */
     private function getCarts()
     {
-        // Legacy method that requires trimming as query strings were previously concatenated.
-        $shopRestriction = trim(str_replace('AND', '', \Shop::addSqlRestriction(\Shop::SHARE_CUSTOMER, 'c')));
+        // Legacy method that requires output modification as query strings were previously concatenated.
+        $shopRestriction =  preg_replace("/^\s+AND\s+/", "", \Shop::addSqlRestriction(\Shop::SHARE_CUSTOMER, 'c'));
 
         $sql = new \DbQuery();
         $sql->select('c.`id_cart`, c.`id_customer`, c.`date_upd`, cu.`firstname`, cu.`lastname`, cu.`email`');
